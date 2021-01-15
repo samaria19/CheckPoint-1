31247dd57d3295df59785c2a18debdb5ce78334e
Jonathan Corley <jcorley@noscihigh.org>
2021-01-15 16:35:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Instructions:
+
+//////////////////////
+///////// Arrays /////
+//////////////////////
+
+/*
+Declare a variable named hobbies and assign it to an array.
+Then, fill the array with string values of your hobbies
+*/
+
+
+
+
+/////////////////////////
+/////// Arrays Pt 2 /////
+/////////////////////////
+
+/*
+Declare a variable named dataTypes and assign it to an array.
+
+Fill the array with values of all data types we have learned so far, including arrays!
+
+Data Types include: String, Number, Boolean, and Array.
+*/
